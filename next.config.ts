06f8502d5e62437afac0b216c4fc50fbfe4aaf5b
BLK LUV [org] <info@blkluv.org>
2025-04-21 08:28:57 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Add any other config options below
 };
 
 export default nextConfig;
+
